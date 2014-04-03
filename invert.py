3bb753fc6e3605d422f55cf3bf6de380a224ca70
Eric Kern <ekern@mitre.org>
2014-04-03 13:23:03 -0400

    Added support for lists

--- invert.py
+++ invert.py
@@ -1,5 +1,6 @@
-import inspect
 import ast
+import copy
+import inspect
 
 FLIP_MAP = {
         'a' : 'ɐ',
@@ -57,6 +58,24 @@ FLIP_MAP = {
         '_' : '‾',
         '!' : '¡'}
 
+def flip(var):
+    # flip strings
+    if isinstance(var, str):
+        return flip_string(var)
+
+    # flip multi-dimensional arrays
+    try:
+        new_var = copy.copy(var)
+        for index, row in enumerate(var):
+            new_var[index] = row[::-1]
+
+        return new_var
+    except:
+        pass
+    
+    # can't be flipped
+    return var
+
 def flip_string(s):
     return ''.join([FLIP_MAP[c] if c in FLIP_MAP else c for c in s])
 
@@ -74,17 +93,37 @@ def add_inverts(var):
     if var_name is None:
         raise ValueError('Could not find varaible')
 
-    caller[0].f_globals[flip_string(var_name)] = flip_string(var)
+    caller[0].f_globals[flip_string(var_name)] = flip(var)
     caller[0].f_globals[var_name[::-1]] = var[::-1]
-    caller[0].f_globals[flip_string(var_name[::-1])] = flip_string(var[::-1])
+    caller[0].f_globals[flip_string(var_name[::-1])] = flip(var[::-1])
 
 def main():
     mystring = 'Hello World!'
     add_inverts(mystring)
 
+    print(mystring)
     print(ɯʎsʇɹᴉuƃ)
     print(ƃuᴉɹʇsʎɯ)
     print(gnirtsym)
+    print()
+
+    mylist = [[1,2,3], [4,5,6]]
+    add_inverts(mylist)
+
+    print(mylist)
+    print(ɯʎlᴉsʇ)
+    print(ʇsᴉlʎɯ)
+    print(tsilym)
+    print()
+
+    import numpy
+    mymatrix = numpy.array([[1,2,3],[4,5,6],[7,8,9]])
+    add_inverts(mymatrix)
+
+    print(mymatrix)
+    print(xirtamym)
+    print(ɯʎɯɐʇɹᴉx)
+    print(xᴉɹʇɐɯʎɯ)
 
 if __name__ == '__main__':
     exit(main())
